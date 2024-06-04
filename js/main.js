afa455054d9d8130a78f5c37cbed5f787a54f702
AsadbekBahromov21 <bahromov1621a@gmail.com>
2024-06-04 15:17:51 +0300

    masala

---
+++ js/main.js
@@ -0,0 +1,71 @@
+//{
+   // let birthDate = prompt("tugulgan yillingiz nechi")
+//let age = 2024 - birthDate
+//alert(age)
+//}
+
+
+//  {
+// //     //1 - masala
+// // let a = prompt("a sonini kiriting")
+// // let p = 4 * a
+// // alert(`primetiri: ${p} `)
+//  }
+
+// {
+// //   2- masala
+// let a = prompt("a sonini kiriting")
+// let s = a * a
+// alert(`Yuza: ${s}`)
+// }
+// {
+//     //3 - masala
+//     let a = prompt("a sonono kiriting")
+//     let b = prompt("b sonni kiriting")
+//     let s = a * b
+//     let p = 2 * (a + b)
+//     alert(`Yuzasi: ${s} perimetiri: ${p}`)
+//     //alert(`Peremetiri: ${p}`)
+// }
+
+// {
+//     // 4 - masala
+//     let d = prompt("d sonni kiriting")
+//     let l = 3.14 * d
+//     alert(`Uzunligi: ${l}`)
+// }
+
+// {
+//     // 5 - masala
+//     let a = prompt("a sonni kiriting")
+//     let v = a * (a * a)
+//     let s = 6 * (a * a)
+//     alert(` Hajmi: ${v} Tola sirti: ${s}`)
+// }
+
+// {
+//     // 6 - masala
+//     let a = +prompt("a sonini kiriting")
+//     let b = +prompt("b sonini kiriting")
+//     let c = +prompt("c sonini kiriting")
+//     let v = a * b * c
+//     let s = 2 * (a * b + b * c + a * c)
+//     alert(` Hajmi: ${v} tola sirti ${s}`)
+// }
+
+// {
+// //     // 7 - masala
+// //     let r = prompt("r sonini kiriting")
+// //     let l = 2 * 3.14 * r 
+// //     let s = 3.14 * (r * r)
+// //     alert(`Uzunligi: ${l} Yuzasi: ${s}`)
+//  }
+
+
+//  {
+// //     // 8 - masala
+// //     let a = +prompt("a sonini kiriting")
+// //     let b = +prompt("b sonini kiriting")
+// //     console.log( l = (a + b)/2); 
+// //     alert(`Orta arifmetigi: ${l}`)
+//  }
